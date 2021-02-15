c41c2105b4c6189e63ef0e6d13637de87b183fa2
James Harris <mharris9608@yahoo.com>
2021-02-15 20:38:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,4 +14,27 @@ class Song
     song = self.new
     @@all << song
     song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+    self.find_by_name(name) || self.create_by_name(name)
+
+  end
+
+  end
 end
